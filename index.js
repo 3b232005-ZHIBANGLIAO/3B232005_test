a96536f09df69348302b1c95a4935a330794395e
3B232005 <3B232005@gm.student.ncut.edu.tw>
2025-12-26 03:16:41 +0800

    JavaScript

---
+++ index.js
@@ -0,0 +1,9 @@
+document.title = "企業應用系統開發";
+
+document.getElementById("txt").textContent = "菜菜撈撈";
+
+const p = document.createElement("p");
+p.textContent = "動態文字段落。";
+document.body.appendChild(p);
+
+document.getElementById("heading").innerHTML = "<h1>老天保佑這學期歐趴!!!</h1>"
---
+++ oo.js
@@ -0,0 +1,2 @@
+var user = { name: '廖志邦', age: '20' };
+document.getElementById("user").innerHTML = user.name + "<br>" + user.age + "歲<br>";
